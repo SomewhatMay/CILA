88c60604e835338afd0bf948c80c62b7da17d887
UndefinedN00B <40745122+UndefinedN00B@users.noreply.github.com>
2021-01-04 10:10:01 -0500

    Create main.lua

---
+++ CILA 2.0/main.lua
@@ -0,0 +1,21 @@
+--[[
+
+This scirpt was made by: UMAYEERIBN!
+Thanks for useing my Assistant!
+Hope you like it. See yah!
+
+]]
+
+
+
+--// DO NO CHANGE AFTER THIS LINE \\--
+
+local HTTPS = "https:GetWebsite()"
+local tablenum = "2.0"
+local webAdress = "argos.cila.co/data/table"..tablenum
+local data = HTTPS..webAdress
+if data then end
+function doing()
+	return
+end
+dofile("respond.lua")
